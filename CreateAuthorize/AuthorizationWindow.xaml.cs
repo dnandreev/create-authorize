571d0a78ba8b811e3b094554fef03989405e717b
User <Ligastor.loms@gmail.com>
2021-09-30 15:37:28 +0300

    Add project files.

---
+++ CreateAuthorize/AuthorizationWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace CreateAuthorize{
+	public partial class AuthorizationWindow : Window{
+		DBContainer db;
+
+		public AuthorizationWindow(){
+			InitializeComponent();
+			db = new DBContainer();
+		}
+
+		void AuthorizationClick(object sender, RoutedEventArgs e){
+			if(login.Text == "" || password.Password == "")
+				MessageBox.Show("Ошибка: пустое поле!");
+			else if(db.Users.Select(item => item.Login + " " + item.Password).Contains(login.Text + " " + password.Password))
+				MessageBox.Show("Успех: авторизация!");
+			else
+				MessageBox.Show("Ошибка: неверный логин или пароль!");
+		}
+
+		void RegistrationClick(object sender, RoutedEventArgs e){
+			new RegistrationWindow().Show();
+			Close();
+		}
+	}
+}
---
+++ CreateAuthorize/RegistrationWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace CreateAuthorize{
+	public partial class RegistrationWindow : Window{
+		DBContainer db;
+
+		public RegistrationWindow(){
+			InitializeComponent();
+			db = new DBContainer();
+		}
+
+		void RegistrationClick(object sender, RoutedEventArgs e){
+			if(login.Text == "" || password.Password == "" || lastName.Text == "" || firstName.Text == "" || middleName.Text == "")
+				MessageBox.Show("Ошибка: пустое поле!");
+			else if(db.Users.Select(item => item.Login).Contains(login.Text))
+				MessageBox.Show("Ошибка: логин занят!");
+			else{
+				db.Users.Add(new User(){
+					Login = login.Text,
+					Password = password.Password,
+					LastName = lastName.Text,
+					FirstName = firstName.Text,
+					MiddleName = middleName.Text
+				});
+				db.SaveChanges();
+				MessageBox.Show("Успех: регистрация!");
+				new AuthorizationWindow().Show();
+				Close();
+			}
+		}
+
+		void CancelClick(object sender, RoutedEventArgs e){
+			new AuthorizationWindow().Show();
+			Close();
+		}
+	}
+}
